ebbbabce6055afcac9edf92d6409997e1be017d3
Sir-Iyke <iyke.uwajeh@digital.hmrc.gov.uk>
2021-01-28 11:12:06 +0000

    DL-4897 Removed travis yaml file

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,15 +22,15 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"    % "3.3.0",
-    "uk.gov.hmrc"       %% "auth-client"                   % "3.2.0-play-27",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"    % "3.4.0",
+    "uk.gov.hmrc"       %% "auth-client"                   % "3.3.0-play-27",
     "uk.gov.hmrc"       %% "play-partials"                 % "7.1.0-play-27",
     "uk.gov.hmrc"       %% "domain"                        % "5.10.0-play-27",
     "uk.gov.hmrc"       %% "http-caching-client"           % "9.2.0-play-27",
     "uk.gov.hmrc"       %% "emailaddress"                  % "3.5.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.5.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.5.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                       % "8.21.0-play-27",
-    "com.typesafe.play" %% "play-json-joda"                % "2.7.4",
+    "com.typesafe.play" %% "play-json-joda"                % "2.9.2",
     "uk.gov.hmrc"       %% "govuk-template"                % "5.61.0-play-27"
   )
 
@@ -46,11 +46,11 @@ private object AppDependencies {
         "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"             % scope,
         "org.pegdown"            %  "pegdown"            % "1.6.0"             % scope,
         "org.jsoup"              %  "jsoup"              % "1.13.1"            % scope,
-        "org.scalacheck"         %% "scalacheck"         % "1.14.3"            % scope,
-        "org.mockito"            %  "mockito-core"       % "3.3.3"             % scope,
+        "org.scalacheck"         %% "scalacheck"         % "1.15.2" % scope,
+        "org.mockito"            %  "mockito-core"       % "3.7.7" % scope,
         "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
         "com.github.tomakehurst" %  "wiremock-jre8"      % "2.27.2"            % scope,
-        "uk.gov.hmrc"            %% "bootstrap-test-play-27"  % "2.25.0"       % scope
+        "uk.gov.hmrc"            %% "bootstrap-test-play-27"  % "3.4.0" % scope
       )
     }.test
   }
