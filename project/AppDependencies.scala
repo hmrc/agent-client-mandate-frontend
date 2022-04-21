543f78e3a21565032bd96b2ecd696ae600b5138c
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-04-21 17:40:42 +0100

    DL-7521 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ private object AppDependencies {
     "uk.gov.hmrc"       %% "emailaddress"                  % "3.6.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.11.0-play-28",
     "com.typesafe.play" %% "play-json-joda"                % "2.9.2",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.11.0-play-28"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.14.0-play-28"
   )
 
   trait TestDependencies {
@@ -28,10 +28,10 @@ private object AppDependencies {
         "org.pegdown"                  %  "pegdown"                % "1.6.0"    % scope,
         "org.jsoup"                    %  "jsoup"                  % "1.14.3"   % scope,
         "org.scalatestplus"            %% "scalacheck-1-15"        % "3.2.11.0" % scope,
-        "org.mockito"                  %  "mockito-core"           % "4.4.0"    % scope,
+        "org.mockito"                  %  "mockito-core"           % "4.5.1"    % scope,
         "org.scalatestplus"            %% "scalatestplus-mockito"  % "1.0.0-M2" % scope,
         "com.typesafe.play"            %% "play-test"              % current    % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.32.0"   % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.33.1"   % scope,
         "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.2"   % scope,
         "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "5.20.0"   % scope
       )
