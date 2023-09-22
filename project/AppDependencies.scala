904414f0c320110e2ca1d396c624d007264ca209
Nalinikantha <nr.yammanuru@gmail.com>
2023-09-22 12:53:32 +0100

    DL-11507 Updated CSP Config

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.21.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "7.22.0",
     "uk.gov.hmrc"       %% "play-partials"                 % "8.4.0-play-28",
     "uk.gov.hmrc"       %% "domain"                        % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "http-caching-client"           % "10.0.0-play-28",
     "uk.gov.hmrc"       %% "emailaddress"                  % "3.8.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.20.0-play-28",
     "commons-codec"     %  "commons-codec"                 % "1.16.0"
   )
 
@@ -30,9 +30,9 @@ private object AppDependencies {
         "org.mockito"                  %  "mockito-core"           % "5.5.0"    % scope,
         "org.scalatestplus"            %% "scalatestplus-mockito"  % "1.0.0-M2" % scope,
         "com.typesafe.play"            %% "play-test"              % current    % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.35.0"   % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.35.1"   % scope,
         "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.15.2"   % scope,
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "7.21.0"   % scope
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "7.22.0"   % scope
       )
     }.test
   }
