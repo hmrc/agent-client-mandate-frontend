35ba6daadaaaabfe88142beff17153c4d7f543dc
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-10-03 14:20:40 +0100

    DL-11907 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ private object AppDependencies {
     "uk.gov.hmrc"       %% "http-caching-client"           % "10.0.0-play-28",
     "uk.gov.hmrc"       %% "emailaddress"                  % "3.8.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.20.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "7.21.0-play-28",
     "commons-codec"     %  "commons-codec"                 % "1.16.0"
   )
 
