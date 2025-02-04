e8ee1e82634de567db2937e3fcd258b296e155a6
Michael Olafisoye <molafisoye@gmail.com>
2025-02-04 10:41:48 +0000

    DL-15171 update bootstrap-frontend-play-30 to 9.7.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"            % "9.5.0",
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"            % "9.7.0",
     "uk.gov.hmrc"   %% "play-partials-play-30"                 % "10.0.0",
     "uk.gov.hmrc"   %% "domain-play-30"                        % "10.0.0",
     "uk.gov.hmrc"   %% "http-caching-client-play-30"           % "12.1.0",
@@ -15,7 +15,7 @@ private object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30" % "9.5.0",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-30" % "9.7.0",
     "org.scalatestplus"            %% "scalacheck-1-17"        % "3.2.18.0"
   ).map(_ % Test)
 
