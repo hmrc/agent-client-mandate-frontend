4cc9dda6093911526113e0fe2bbfab6d9e0bf8d7
nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>
2026-08-10 10:48:53 +0100

    removed print and added review comment

--- test/unit/uk/gov/hmrc/agentclientmandate/controllers/agent/NRLQuestionControllerSpec.scala
+++ test/unit/uk/gov/hmrc/agentclientmandate/controllers/agent/NRLQuestionControllerSpec.scala
@@ -132,7 +132,6 @@ class NRLQuestionControllerSpec extends PlaySpec with BeforeAndAfterEach with Mo
         viewWithAuthorisedAgent { result =>
           status(result) must be(OK)
           val document = Jsoup.parse(contentAsString(result))
-          println("**********************************" + document)
           document.title() must be("agent.nrl-question.title - service.name - GOV.UK")
           document.select("h1.govuk-fieldset__heading").text() must include("agent.nrl-question.header")
           document.select("h2.govuk-caption-l > span").text() must include("ated.screen-reader.section agent.add-a-client.sub-header")
